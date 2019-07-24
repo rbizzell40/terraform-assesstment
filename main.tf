179a7934cd162cd21b70109cb9c51cc481ab728b
Rodney Bizzell <hardworker30@gmail.com>
2019-07-23 22:56:37 -0500

    Update main.tf
    remove credentials

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 // Configure the Google Cloud provider
 
 provider "google" {
-  credentials = "${file("test-assesstment-2456-cca023a90c34.json")}"
+  credentials = "${file("credentials.json")}"
   project     = "test-assesstment-2456"
   region      = "us-east1"
 }
